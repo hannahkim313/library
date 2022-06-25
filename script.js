ca5c26b7c12d8a8e30bef190cf2c7c3baa7f1a88
Hannah Kim <devhkim13@gmail.com>
2022-06-24 23:25:09 -0700

    Add and display books into 'My Library'

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ function makeButtonSmall() {
 
 /**
  * Checks that all inputs are filled in before submitting the form.
+ * @returns {Boolean} True if all text inputs are filled.
  */
 function validateForm() {
     let filled = 0;
@@ -72,78 +73,82 @@ function clearForm() {
 
 /**
  * Creates a new book object and stores it into library array.
+ * @param {Boolean} isUserInput - Is book information from user input or not.
  */
-function addBookToLibrary() {
-    const book = new Book(
-        formInputs[0].value,
-        formInputs[1].value,
-        formInputs[2].value,
-        formInputs[3].checked
-    );
-    library.push(book);
+function addBookToLibrary(isUserInput) {
+    if (isUserInput === false) {
+        const bookOne = new Book("The Da Vinci Code", "Dan Brown", "689", true);
+        const bookTwo = new Book("To Kill a Mockingbird", "Harper Lee", "281", true);
+        const bookThree = new Book("The Giver", "Lois Lowry", "240", false);
+        library.push(bookOne, bookTwo, bookThree);
+    } else {
+        const book = new Book(
+            formInputs[0].value,
+            formInputs[1].value,
+            formInputs[2].value,
+            formInputs[3].checked
+        );
+        library.push(book);
+    }
 }
 
 /**
  * Displays newly added book into "My Library."
+ * @param {Boolean} isUserInput - Is book information from user input or not.
  */
-function displayBook() {
-    let index = 0;
-    for (const book of library) {
-        if (index === library.length - 1) {
-            const article = document.createElement("article");
-        
-            const info = document.createElement("div");
-            const title = document.createElement("h3");
-            const em = document.createElement("em");
-            const author = document.createElement("p");
-            const pages = document.createElement("p");
-        
-            const action = document.createElement("div");
-            const checkboxContainer = document.createElement("div");
-            const text = document.createElement("p");
-            const checkbox = document.createElement("input");
-            const label = document.createElement("label");
-            const deleteBtn = document.createElement("button");
-            const deleteImg = document.createElement("img");
-        
-            article.setAttribute("class", "book");
-            info.setAttribute("class", "info");
-            action.setAttribute("class", "action");
+function displayBook(isUserInput) {
+    for (let i = 0; i < library.length; i++) {
+        if (isUserInput === true) i = library.length - 1;
+        const article = document.createElement("article");
+        const info = document.createElement("div");
+        const title = document.createElement("h3");
+        const em = document.createElement("em");
+        const author = document.createElement("p");
+        const pages = document.createElement("p");
 
-            checkboxContainer.setAttribute("class", "checkbox-container");
-            checkbox.setAttribute("type", "checkbox");
-            checkbox.setAttribute("id", `toggle_${library.length}`);
-            checkbox.setAttribute("name", "read_status");
-            checkbox.setAttribute("class", "checkbox");
-            if (book.hasRead === true) checkbox.setAttribute("checked", "true");
-            label.setAttribute("for", `toggle_${library.length}`);
-            label.setAttribute("class", "switch");
-        
-            deleteImg.src = "img/delete.svg";
-            deleteImg.alt = "Trash can icon";
-        
-            em.textContent = `${book.title}`;
-            author.textContent = `Written by ${book.author}`;
-            pages.textContent = `${book.pages} pages`;
-            text.textContent = "Read:";
-            
-            title.appendChild(em);
-            info.appendChild(title);
-            info.appendChild(author);
-            info.appendChild(pages);
+        const action = document.createElement("div");
+        const checkboxContainer = document.createElement("div");
+        const text = document.createElement("p");
+        const checkbox = document.createElement("input");
+        const label = document.createElement("label");
+        const deleteBtn = document.createElement("button");
+        const deleteImg = document.createElement("img");
 
-            checkboxContainer.appendChild(text);
-            checkboxContainer.appendChild(checkbox);
-            checkboxContainer.appendChild(label);
-            deleteBtn.appendChild(deleteImg);
-            action.appendChild(checkboxContainer);
-            action.appendChild(deleteBtn);
+        article.setAttribute("class", "book");
+        info.setAttribute("class", "info");
+        action.setAttribute("class", "action");
 
-            article.appendChild(info);
-            article.appendChild(action);
-            booksContainer.appendChild(article);
-        }
-        index++;
+        checkboxContainer.setAttribute("class", "checkbox-container");
+        checkbox.setAttribute("type", "checkbox");
+        checkbox.setAttribute("name", "read_status");
+        checkbox.setAttribute("class", "checkbox");
+        if (library[i].hasRead === true) checkbox.setAttribute("checked", "true");
+        label.setAttribute("for", `toggle_${library.length}`);
+        label.setAttribute("class", "switch");
+
+        deleteImg.src = "img/delete.svg";
+        deleteImg.alt = "Trash can icon";
+
+        em.textContent = `${library[i].title}`;
+        author.textContent = `Written by ${library[i].author}`;
+        pages.textContent = `${library[i].pages} pages`;
+        text.textContent = "Read:";
+
+        title.appendChild(em);
+        info.appendChild(title);
+        info.appendChild(author);
+        info.appendChild(pages);
+
+        checkboxContainer.appendChild(text);
+        checkboxContainer.appendChild(checkbox);
+        checkboxContainer.appendChild(label);
+        deleteBtn.appendChild(deleteImg);
+        action.appendChild(checkboxContainer);
+        action.appendChild(deleteBtn);
+
+        article.appendChild(info);
+        article.appendChild(action);
+        booksContainer.appendChild(article);
     }
 }
 
@@ -152,6 +157,8 @@ function displayBook() {
  ******************************************************************************/
 
 window.addEventListener("pageshow", e => {
+    addBookToLibrary(false);
+    displayBook(false);
     form.style.display = "none";
     if (mediaQuery.matches) makeButtonSmall();
 });
@@ -170,8 +177,8 @@ closeBtn.addEventListener("click", e => form.style.display = "none");
 
 submitBtn.addEventListener("click", e => {
     if (validateForm() === true) {
-        addBookToLibrary();
-        displayBook();
+        addBookToLibrary(true);
+        displayBook(true);
         e.preventDefault();
     }
 });
@@ -187,5 +194,4 @@ submitBtn.addEventListener("click", e => {
  *    when confirmed to delete book, remove it from "My Library."
  * 3. Update reading log when new book is added.
  * 4. Update reading log when user changes read status of a book.
- * 5. Add 3 default books created in HTML into library array for manipulation.
  */
